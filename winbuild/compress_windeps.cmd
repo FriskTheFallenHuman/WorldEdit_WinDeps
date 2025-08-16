57162413702a37bb846563f584c4384a72d0eb87
Krispy <nikodash2020@hotmail.com>
2025-08-16 15:33:02 -0500

    Update build bat files to try and locate a local installation of 7zip/NanaZip

--- winbuild/compress_windeps.cmd
+++ winbuild/compress_windeps.cmd
@@ -1,2 +1,22 @@
+@echo off
+setlocal
+
+:: Check if 7z is installed
+where /q 7z
+if %errorlevel% neq 0 (
+    echo 7z is not installed, trying to locate Nanazip
+    where /q Nanazip
+    if %errorlevel% neq 0 (
+        echo Nanazip is not found, please install either 7z or Nanazip
+        exit /b
+    ) else (
+        echo Nanazip is installed, zipping the folders
+		NanaZipC a -r -mmt2 -mx9 -x!*.ilk -x!*.exp -x!*.pdb windeps.7z w32deps/* w64deps/* winincludes/*
+        exit /b
+    )
+)
+
 cd ..\
-"c:\Program Files\7-Zip\7z.exe" a -r -mmt2 -mx9 -x!*.ilk -x!*.exp -x!*.pdb windeps.7z w32deps/* w64deps/*
+
+:: If 7z is installed, zip the folders
+7z a -r -mmt2 -mx9 -x!*.ilk -x!*.exp -x!*.pdb windeps.7z w32deps/* w64deps/* winincludes/*
--- winbuild/compress_windeps_pdbs.cmd
+++ winbuild/compress_windeps_pdbs.cmd
@@ -1,2 +1,22 @@
+@echo off
+setlocal
+
+:: Check if 7z is installed
+where /q 7z
+if %errorlevel% neq 0 (
+    echo 7z is not installed, trying to locate Nanazip
+    where /q Nanazip
+    if %errorlevel% neq 0 (
+        echo Nanazip is not found, please install either 7z or Nanazip
+        exit /b
+    ) else (
+        echo Nanazip is installed, zipping the folders
+		NanaZipC a -r -mx9 windeps.pdbs.7z w32deps/*.pdb w64deps/*.pdb
+        exit /b
+    )
+)
+
 cd ..\
-"c:\Program Files\7-Zip\7z.exe" a -r -mx9 windeps.pdbs.7z w32deps/*.pdb w64deps/*.pdb
+
+:: If 7z is installed, zip the folders
+7z a -r -mx9 windeps.pdbs.7z w32deps/*.pdb w64deps/*.pdb
